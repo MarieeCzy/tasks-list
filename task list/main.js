5632aece6dc8b1cba6c9788954168d3ebc8b6cc5
Maria Cz <maria.czapczyk@gmail.com>
2022-02-15 10:09:00 +0100

    Add local storage

--- task list/main.js
+++ task list/main.js
@@ -1,27 +1,37 @@
-const btnAdd = document.querySelector('.add');
-const taskList = document.querySelector('.tasks-wrapper');
+const btnAdd = document.querySelector(".add");
+const taskList = document.querySelector(".tasks-wrapper");
 
-btnAdd.addEventListener('click', createNewTask);
+btnAdd.addEventListener("click", createNewTask);
+
+let taskContentText = "Something different";
+let imgToDo = `<img src="./Img/akar-icons_check-box-filldone-btn.svg" alt="done icon">`;
+let imgDone = `<img src="./Img/akar-icons_check-box-filldone.svg" alt="done icon">`;
 
 function createNewTask() {
-    let task = document.createElement("div");
-    taskList.appendChild(task);
-    task.classList.add("task");
+  let task = document.createElement("div");
+  taskList.appendChild(task);
+  task.classList.add("task");
 
-    let taskContent = document.createElement("div");
-    task.appendChild(taskContent);
-    taskContent.classList.add("task-content");
+  task.innerHTML = `<div class="task-content">
+    <p class="task-content__text">${taskContentText}</p></div>
+<div class="check-mark">${imgToDo}
+</div>`;
 
-    let taskContentText = document.createElement("p");
-    taskContent.appendChild(taskContentText);
-    taskContentText.classList.add("task-content__text")
+  window.localStorage.setItem("task", JSON.stringify(`${taskContentText}`));
 
-    taskContentText.innerHTML = "New task added to list";
+  // let taskContent = document.createElement("div");
+  // task.appendChild(taskContent);
+  // taskContent.classList.add("task-content");
 
+  // let taskContentText = document.createElement("p");
+  // taskContent.appendChild(taskContentText);
+  // taskContentText.classList.add("task-content__text")
 
-    let checkMark = document.createElement("div");
-    task.insertAdjacentElement("beforeend", checkMark)
+  // taskContentText.innerHTML = "New task added to list";
 
-    checkMark.classList.add("check-mark");
-    checkMark.innerHTML = "<img src='./Img/akar-icons_check-box-filldone-btn.svg'>";
-};
+  // let checkMark = document.createElement("div");
+  // task.insertAdjacentElement("beforeend", checkMark)
+
+  // checkMark.classList.add("check-mark");
+  // checkMark.innerHTML = "<img src='./Img/akar-icons_check-box-filldone-btn.svg'>";
+}
