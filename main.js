97bf720ec43ee9e7fa26fe3a2b90cd0a3e4182b5
Maria Cz <maria.czapczyk@gmail.com>
2022-02-17 16:23:24 +0100

    Add keypres, "delete all" function, comments.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const btnAdd = document.getElementById("add-button");
-const btnDel = document.getElementById(".delete-button");
+const btnDel = document.getElementById("delete-button");
 const taskContainer = document.querySelector(".tasks-wrapper");
-
 
 const imgDone = `<img src="./Img/akar-icons_check-box-filldone-btn.svg" alt="done icon">`;
 const imgToDo = `<img src="./Img/akar-icons_check-box-filldone.svg" alt="done icon">`;
+
+//create list of all tasks
 
 const addTaskToList = () => {
   const taskList = document.querySelectorAll(".task");
@@ -16,6 +17,7 @@ const addTaskToList = () => {
   }
 };
 
+//add new task to document
 
 const createNewTask = () => {
   const task = document.createElement("div");
@@ -27,12 +29,27 @@ const createNewTask = () => {
 <div class="check-mark button">${imgToDo}
 </div>`;
 
+  focusMethod = getFocus;
+
   addTaskToList();
 };
 
+function getFocus() {
+  const lastTask = taskContainer.lastElementChild;
+  const inputActive = lastTask.querySelector(".task-content__text");
+  inputActive.focus();
+}
+
+document.addEventListener("keypress", (e) => {
+  if (e.keyCode === 13) {
+    createNewTask();
+    getFocus();
+  }
+});
+
 btnAdd.addEventListener("click", createNewTask);
 
-
+//mark task as done or to do
 
 function taskDone() {
   const finishedTask = this.parentNode;
@@ -43,12 +60,16 @@ function taskDone() {
     finishedTask.lastElementChild.innerHTML = `${imgDone}`;
   } else {
     finishedTask.lastElementChild.innerHTML = `${imgToDo}`;
-  };
+  }
 
+  btnDel.addEventListener("click", delAllFinished);
 }
 
+//delete all finished tasks
 
-
-
-
-// window.localStorage.setItem("task", JSON.stringify(`${taskContentText}`));
+function delAllFinished() {
+  const allToDel = document.querySelectorAll(".task-finished");
+  for (const el of allToDel) {
+    el.parentNode.remove();
+  }
+}
